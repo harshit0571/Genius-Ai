d852861e57f42fb70ad75ecbd6ccb0bf34e04783
harshit0571 <harshitchadha65@gmail.com>
2023-07-23 23:03:01 +0530

    clerk auth added

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
-export default function Home() {
+export default function DashBoardPage() {
   return (
     <div>
-      <Button>gello</Button>
+      <Button>dashboard</Button>
     </div>
   );
 }
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <p>landing page</p>
+      <div>
+        <Link href="/sign-in">
+          <Button>Login</Button>
+        </Link>
+
+        <Link href="/sign-up">
+          <Button>Register</Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
